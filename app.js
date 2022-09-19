a319d66e62ab43a1eeef43a525dabea3e9cf747f
Vivek-1012 <110529479+Vivek-1012@users.noreply.github.com>
2022-09-19 16:28:05 +0530

    doc
    Handle suggestion done..

--- app.js
+++ app.js
@@ -14,18 +14,34 @@ function submitHandler() {
   calculateProfitAndLoss(ip, qty, curr);
 }
 
-function calculateProfitAndLoss(initial, quantity, current) {
-    if (initial > current) {
+function calculateProfitAndLoss(initial, quantity, current) {  if(initialPrice.value < 0){
+  outputBox.innerText=("😑Don't insert negative value in Initial Price😑");
+} 
+else if (stocksQuantity.value < 0 ) {
+  outputBox.innerText=("😑Don't insert negative value in Quantity of Stocks😑");
+}
+ else if(currentPrice.value < 0 ){
+  outputBox.innerText=("😑Don't insert negative value in Current Price😑");
+ }else if( initialPrice.value == 0){
+  outputBox.innerText=("🤨 Don't leave value of Initial price empty 🤨")
+ } else if( stocksQuantity.value == 0){
+  outputBox.innerText=("🤨Don't leave value of quantity of Stock empty🤨")
+ }else if( currentPrice.value == 0){
+  outputBox.innerText=("🤨Don't leave value of current price empty🤨")
+ }
+
+
+   else  if (initial > current) {
         //loss Logic here
         var loss = (initial - current) * quantity;
         var lossPercentge = (loss / initial) * 100;
 
-        showOutput(`Hey the loss is ${loss} and the percente is ${lossPercentge}%`);
+        showOutput(`🤒 Hey the loss is ${loss} and the percente is ${lossPercentge}% 🤒`);
     } else if (current > initial) {
         var profit = (current - initial) * quantity;
         var profitPercentage = (profit / initial) * 100; 
 
-        showOutput(`Hey the profit is ${profit} and the percentage is ${profitPercentage}%`);
+        showOutput(`🤩 Hey the profit is ${profit} and the percentage is ${profitPercentage}% 🤩` );
 
    } else {
         showOutput(`No pain no gain and no gain no pain`);
@@ -34,5 +50,6 @@ function calculateProfitAndLoss(initial, quantity, current) {
 
         outputBox.innerHTML = message;
       }
-
     }
+
+    
